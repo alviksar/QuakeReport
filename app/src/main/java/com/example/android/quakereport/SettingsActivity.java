f5aab81f2cf1adaeabf430babd62ec855e3fba62
Alexey Sarafannikov <alviksar@gmail.com>
2017-10-12 17:12:47 +0300

    Add onPreferenceChange to EarthquakePreferenceFragment

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -10,14 +13,36 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitudePreference = findPreference(getString(R.string.settings_min_magnitude_key));
+         //  bindPreferenceSummaryToValue(minMagnitudePreference);
+            minMagnitudePreference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(minMagnitudePreference.getContext());
+            String preferenceString = sharedPreferences.getString(minMagnitudePreference.getKey(), "");
+            onPreferenceChange(minMagnitudePreference, preferenceString);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
